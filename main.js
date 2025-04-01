b038ddcde10c0b9c860157e15d84339ea62a2bde
gui7sampaio <gui7sampaio@gmail.com>
2025-04-01 05:17:54 +0000

    recebendo a porcentagem da gorgeta em todos os botoes

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ let tipPercentage = 0
 let numberOfPeople = 0
 let buttonSelected = null
 
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", receiveBillValue)
+
+let numberOfPeopleInput = document.querySelector("#people")
+numberOfPeopleInput.addEventListener("input", receiveNumberOfPeopleValue)
+
 function receiveBillValue() {
     bill = billInput.valueAsNumber
 
@@ -13,4 +19,32 @@ function receiveNumberOfPeopleValue() {
     numberOfPeople = numberOfPeopleInput.valueAsNumber
 
     calculate()
+}
+
+function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100
+
+    removeClassButtonSelected()
+
+    document.querySelector("#custom-tip").value = ""
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+
+    calculate()
+}
+
+function receiveCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+
+    removeClassButtonSelected()
+
+    calculate()
+}
+
+function removeClassButtonSelected() {
+    if(buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
 }
