83660609b0194461e703ecd52222b836d8fa6d0e
gui7sampaio <gui7sampaio@gmail.com>
2025-04-01 05:18:53 +0000

    funcao para receber o valor custom do botao

--- main.js
+++ main.js
@@ -48,3 +48,15 @@ function removeClassButtonSelected() {
         buttonSelected = null
     }
 }
+
+function calculate() {
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let tipAmountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = bill * tipPercentage / numberOfPeople
+        tipAmountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    }
+}
